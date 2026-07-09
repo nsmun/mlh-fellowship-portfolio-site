5d4da356acf0bc559e65f539313a87df5372b14c
nsmun <sn9943389@gmail.com>
2026-07-09 18:04:39 +0100

    edited .env and init

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
+mydp = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306
+)
+
 nav_pages = [
     {"name": "Home", "url": "/"},
     {"name": "Mahashri", "url": "/maha"},
