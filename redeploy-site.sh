3ae57ef10461eac27eda9ab6dbebfd6fddfff1c9
Jordan-Aniuzu <jordomemordo@gmail.com>
2026-07-10 12:14:43 +0000

    Update redeploy script to use systemd service

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,19 +3,17 @@ set -e
 
 PROJECT_DIR="$HOME/mlh-fellowship-portfolio-site"
 VENV_DIR="venv"
-TMUX_SESSION="flask-server"
-
-tmux kill-server 2>/dev/null || true
 
 cd "$PROJECT_DIR"
 
 git fetch && git reset origin/main --hard
 
 source "$VENV_DIR/bin/activate"
+
 pip install -r requirements.txt
+
 deactivate
 
-tmux new-session -d -s "$TMUX_SESSION" \
-  "cd $PROJECT_DIR && source $VENV_DIR/bin/activate && flask run --host=0.0.0.0"
+sudo systemctl restart myportfolio
 
-echo "Redeploy complete. Flask server running in tmux session '$TMUX_SESSION'."
+echo "Redeploy complete. myportfolio service restarted."
