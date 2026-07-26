1cfbb7508c483378f17b1dd75cde6c316992e6ad
nsmun <sn9943389@gmail.com>
2026-07-26 08:55:33 +0000

    updated redeploy.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -5,5 +5,5 @@ cd mlh-fellowship-portfolio-site/
 
 git fetch && git reset origin/new_stuff --hard
 
-docker compose down
+docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
