ed219155632a0164d2889e6a1795dbc7f60bfe52
nsmun <sn9943389@gmail.com>
2026-07-26 08:35:25 +0000

    new

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -5,7 +5,5 @@ cd mlh-fellowship-portfolio-site/
 
 git fetch && git reset origin/new_stuff --hard
 
-source venv/bin/activate
-pip install -r requirements.txt
-
-sudo systemctl restart myportfolio
+docker compose down
+docker compose -f docker-compose.prod.yml up -d --build
